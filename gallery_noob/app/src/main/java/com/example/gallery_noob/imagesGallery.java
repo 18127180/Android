5da1c5d7a1d1b02404348526c93e5d02fa9b3efd
Le Hoang Phuc <phucyugi1@gmail.com>
2021-03-24 22:42:12 +0700

    load được video nhưng k phải thumbnail

--- gallery_noob/app/src/main/java/com/example/gallery_noob/imagesGallery.java
+++ gallery_noob/app/src/main/java/com/example/gallery_noob/imagesGallery.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.MediaStore;
+import android.util.Log;
+
+import androidx.loader.content.CursorLoader;
 
 import java.util.ArrayList;
 
@@ -15,14 +18,39 @@ public class imagesGallery {
         ArrayList<String> lisOfAllImages=new ArrayList<>();
         String absolutePath;
         uri= MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
-        String[] projection={MediaStore.MediaColumns.DATA,MediaStore.Images.Media.BUCKET_DISPLAY_NAME};
-        String orderBy=MediaStore.Video.Media.DATE_TAKEN;
-        cursor=context.getContentResolver().query(uri,projection,null,null,orderBy+" DESC");
+//        String[] projection={MediaStore.MediaColumns.DATA,MediaStore.Images.Media.BUCKET_DISPLAY_NAME};
+        String[] projection = {
+                MediaStore.Files.FileColumns._ID,
+                MediaStore.Files.FileColumns.DATA,
+                MediaStore.Files.FileColumns.DATE_ADDED,
+                MediaStore.Files.FileColumns.MEDIA_TYPE,
+                MediaStore.Files.FileColumns.MIME_TYPE,
+                MediaStore.Files.FileColumns.TITLE
+        };
+//        String orderBy=MediaStore.Video.Media.DATE_TAKEN;
+//        cursor=context.getContentResolver().query(uri,projection,null,null,orderBy+" DESC");
+        String selection = MediaStore.Files.FileColumns.MEDIA_TYPE + "="
+                + MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE
+                + " OR "
+                + MediaStore.Files.FileColumns.MEDIA_TYPE + "="
+                + MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO;
+
+        Uri queryUri = MediaStore.Files.getContentUri("external");
+        CursorLoader cursorLoader = new CursorLoader(
+                context,
+                queryUri,
+                projection,
+                selection,
+                null, // Selection args (none).
+                MediaStore.Files.FileColumns.DATE_ADDED + " DESC" // Sort order.
+        );
+        cursor = cursorLoader.loadInBackground();
         column_index_data=cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
 
         while(cursor.moveToNext())
         {
             absolutePath=cursor.getString(column_index_data);
+            Log.e("Path",absolutePath);
             lisOfAllImages.add(absolutePath);
         }
         return  lisOfAllImages;
