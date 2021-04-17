d72d158c601895562ce0f2972cecf61c7bebbf50
Noobuster <nhanluu838@gmail.com>
2021-04-17 14:11:48 +0700

    edit

--- gallery_noob/app/src/main/java/com/example/gallery_noob/FullScreenImage.java
+++ gallery_noob/app/src/main/java/com/example/gallery_noob/FullScreenImage.java
@@ -14,6 +14,7 @@ import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.DisplayMetrics;
 import android.util.Log;
@@ -29,6 +30,7 @@ import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -50,12 +52,34 @@ import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FullScreenImage extends AppCompatActivity {
+import ly.img.android.pesdk.PhotoEditorSettingsList;
+import ly.img.android.pesdk.assets.filter.basic.FilterPackBasic;
+import ly.img.android.pesdk.assets.font.basic.FontPackBasic;
+import ly.img.android.pesdk.assets.frame.basic.FramePackBasic;
+import ly.img.android.pesdk.assets.overlay.basic.OverlayPackBasic;
+import ly.img.android.pesdk.assets.sticker.emoticons.StickerPackEmoticons;
+import ly.img.android.pesdk.assets.sticker.shapes.StickerPackShapes;
+import ly.img.android.pesdk.backend.model.EditorSDKResult;
+import ly.img.android.pesdk.backend.model.state.LoadSettings;
+import ly.img.android.pesdk.backend.model.state.PhotoEditorSaveSettings;
+import ly.img.android.pesdk.backend.model.state.manager.SettingsList;
+import ly.img.android.pesdk.ui.activity.EditorBuilder;
+import ly.img.android.pesdk.ui.model.state.UiConfigFilter;
+import ly.img.android.pesdk.ui.model.state.UiConfigFrame;
+import ly.img.android.pesdk.ui.model.state.UiConfigOverlay;
+import ly.img.android.pesdk.ui.model.state.UiConfigSticker;
+import ly.img.android.pesdk.ui.model.state.UiConfigText;
+import ly.img.android.pesdk.ui.panels.item.PersonalStickerAddItem;
+import ly.img.android.pesdk.ui.utils.PermissionRequest;
+import ly.img.android.serializer._3.IMGLYFileWriter;
+
+public class FullScreenImage extends AppCompatActivity implements PermissionRequest.Response {
     static LinearLayout function_bar;
     static LinearLayout title_bar;
 
     static String position;
     private static final int REQUEST_PERMISSIONS = 100;
+    public static int PESDK_RESULT = 1;
     static boolean req = false;
     Button button;
     static boolean visibility;
@@ -74,9 +98,18 @@ public class FullScreenImage extends AppCompatActivity {
     int cur_select;
 
     TextView send,imageMore;
-    TextView del;
+    TextView del,edit;
     static TextView fav;
     static boolean favStatus;
+
+    @Override
+    public void permissionGranted() {}
+
+    @Override
+    public void permissionDenied() {
+        /* TODO: The Permission was rejected by the user. The Editor was not opened,
+         * Show a hint to the user and try again. */
+    }
 
     public static boolean isImageFile(String path) {
         String mimeType = URLConnection.guessContentTypeFromName(path);
@@ -223,6 +256,18 @@ public class FullScreenImage extends AppCompatActivity {
                 } catch (FileNotFoundException e) {
                     e.printStackTrace();
                 }
+            }
+        });
+        //-----------------------edit-------------------------
+        edit = (TextView)findViewById(R.id.edit);
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int cur = viewPager.getCurrentItem();
+                String filename = listOfPathImages.get(cur);
+                Uri uri = FileProvider.getUriForFile(getApplicationContext(),"com.example.gallery_noob",new File(filename));
+//                Toast.makeText(getApplicationContext(),uri.toString(),Toast.LENGTH_SHORT).show();
+                openEditor(uri);
             }
         });
 
@@ -414,6 +459,7 @@ public class FullScreenImage extends AppCompatActivity {
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        PermissionRequest.onRequestPermissionsResult(requestCode, permissions, grantResults);
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
         switch (requestCode) {
@@ -577,5 +623,85 @@ public class FullScreenImage extends AppCompatActivity {
             Log.e("ERROR","turn visible");
         }
         visibility = !visibility;
+    }
+
+    //-------------------------------------------edit-----------------------------------------------
+
+    private SettingsList createPesdkSettingsList() {
+
+        // Create a empty new SettingsList and apply the changes on this referance.
+        PhotoEditorSettingsList settingsList = new PhotoEditorSettingsList();
+
+        // If you include our asset Packs and you use our UI you also need to add them to the UI,
+        // otherwise they are only available for the backend
+        // See the specific feature sections of our guides if you want to know how to add our own Assets.
+
+        settingsList.getSettingsModel(UiConfigFilter.class).setFilterList(
+                FilterPackBasic.getFilterPack()
+        );
+
+        settingsList.getSettingsModel(UiConfigText.class).setFontList(
+                FontPackBasic.getFontPack()
+        );
+
+        settingsList.getSettingsModel(UiConfigFrame.class).setFrameList(
+                FramePackBasic.getFramePack()
+        );
+
+        settingsList.getSettingsModel(UiConfigOverlay.class).setOverlayList(
+                OverlayPackBasic.getOverlayPack()
+        );
+
+        settingsList.getSettingsModel(UiConfigSticker.class).setStickerLists(
+                new PersonalStickerAddItem(),
+                StickerPackEmoticons.getStickerCategory(),
+                StickerPackShapes.getStickerCategory()
+        );
+
+        return settingsList;
+    }
+
+    private void openEditor(Uri inputImage) {
+        SettingsList settingsList = createPesdkSettingsList();
+
+        // Set input image
+        settingsList.getSettingsModel(LoadSettings.class).setSource(inputImage);
+
+        settingsList.getSettingsModel(PhotoEditorSaveSettings.class).setOutputToGallery(Environment.DIRECTORY_DCIM);
+
+        new EditorBuilder(this)
+                .setSettingsList(settingsList)
+                .startActivityForResult(this, PESDK_RESULT);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (resultCode == RESULT_OK && requestCode == PESDK_RESULT) {
+            // Editor has saved an Image.
+            EditorSDKResult data = new EditorSDKResult(intent);
+
+            // This adds the result and source image to Android's gallery
+            data.notifyGallery(EditorSDKResult.UPDATE_RESULT & EditorSDKResult.UPDATE_SOURCE);
+
+            Log.i("PESDK", "Source image is located here " + data.getSourceUri());
+            Log.i("PESDK", "Result image is located here " + data.getResultUri());
+
+            // TODO: Do something with the result image
+
+            // OPTIONAL: read the latest state to save it as a serialisation
+            SettingsList lastState = data.getSettingsList();
+            new IMGLYFileWriter(lastState).writeJson(new File(
+                    Environment.getExternalStorageDirectory(),
+                    "serialisationReadyToReadWithPESDKFileReader.json"
+            ));
+
+        } else if (resultCode == RESULT_CANCELED && requestCode == PESDK_RESULT) {
+            // Editor was canceled
+            EditorSDKResult data = new EditorSDKResult(intent);
+
+            Uri sourceURI = data.getSourceUri();
+            // TODO: Do something with the source...
+        }
     }
 }
